d90c60846a22fe0d1ba61a2d03872d7469f3e7bb
nddszy <1836107093@qq.com>
2022-11-13 12:55:27 +0800

    加入文件

---
+++ HelloWPF/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWPF
+{
+    [TypeConverter(typeof(NameToHumanTypeConverter))]
+    public class Human
+    {
+        public string? Name { get; set; }
+        //在.Net6中，声明的引用类型必须给初始值，如果不想给就需要加上问号，声明成可空类型
+        public Human? Child { get; set; }
+    }
+}
---
+++ HelloWPF/NameToHumanTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWPF
+{
+    public class NameToHumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object? value)
+        {
+            Human child = new Human();
+            if (value != null)
+            {
+                string? name = value.ToString();
+                child.Name = name;
+            }
+            return child;
+        }
+    }
+}
